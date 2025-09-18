6c988519817058857b161ad671ce11ddf5cd1427
KAKUNURI SRILAKSHMI <139469183+Kakunuri-Srilakshmi@users.noreply.github.com>
2025-09-18 17:05:11 +0530

    Update Process-Employees.py

--- Sep - 18 /Practices/Process-Employees.py
+++ Sep - 18 /Practices/Process-Employees.py
@@ -1,1 +1,16 @@
+import csv
 
+with open("employees.csv", "r") as file:
+    reader = csv.DictReader(file)
+    employees = list(reader)
+
+print("Employees:")
+for e in employees:
+    print(f"{e['id']} - {e['name']} ({e['department']}) - rs.{e['salary']}")
+
+
+salaries = [int(e["salary"]) for e in employees]
+total_salary = sum(salaries)
+average_salary = total_salary / len(salaries)
+print(f"\nTotal Salary: rs.{total_salary}")
+print(f"Average Salary: rs.{average_salary: .2f}")
