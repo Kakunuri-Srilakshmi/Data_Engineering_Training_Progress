bc441759ad873d6a3ca4abf74836263c07266d83
KAKUNURI SRILAKSHMI <139469183+Kakunuri-Srilakshmi@users.noreply.github.com>
2025-09-18 17:06:23 +0530

    Update Process-Students.py

--- Sep - 18 /Practices/Process-Students.py
+++ Sep - 18 /Practices/Process-Students.py
@@ -1,1 +1,19 @@
+import json
 
+#open and load json file
+with open("students.json", "r") as file:
+    students = json.load(file)
+
+
+# print all student names
+print("All Students:")
+for s in students:
+    print("-", s["name"])
+
+
+#calculate total & average marks for each student
+print("\nMarks Summary:")
+for s in students:
+    total = sum(s["marks"].values())
+    avg = total / len(s["marks"])
+    print(f"{s['name']} -> Total: {total}, Average: {avg:.2f}")
