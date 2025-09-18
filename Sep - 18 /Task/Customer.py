228d766f59109685a3ea1d49c7fc1996e1fe70d5
KAKUNURI SRILAKSHMI <139469183+Kakunuri-Srilakshmi@users.noreply.github.com>
2025-09-18 17:09:46 +0530

    Update Customer.py

--- Sep - 18 /Task/Customer.py
+++ Sep - 18 /Task/Customer.py
@@ -1,1 +1,17 @@
+# Define a simple class
+class Customer :
+    def __init__(self, first_name, last_name):  #constructor
+        self.first_name = first_name
+        self.last_name = last_name
 
+    def greet(self):  #method
+        return f"Hello, My name is {self.first_name} {self.last_name}"
+
+#create objects of the class
+
+c1 = Customer("Sri", "Lakshmi")
+c2 = Customer("Ayyal", "Reddy")
+
+#call methods
+print(c1.greet())
+print(c2.greet())
