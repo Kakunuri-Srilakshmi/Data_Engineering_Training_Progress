41c0acd6f77cab3eadb3fbdf83f8818584b0528d
KAKUNURI SRILAKSHMI <139469183+Kakunuri-Srilakshmi@users.noreply.github.com>
2025-09-18 17:10:58 +0530

    Update Calculator.py

--- Sep - 18 /Task/Calculator.py
+++ Sep - 18 /Task/Calculator.py
@@ -1,1 +1,23 @@
+# Define calculator class
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
+#create object of calculator
+calc = Calculator()
+
+#call methods
+print("Addition:", calc.add(10,10))
+print("Subtraction:", calc.sub(10,8))
+print("Multiplication:", calc.mul(10,5))
+print("Division:", calc.div(10,2))
 
