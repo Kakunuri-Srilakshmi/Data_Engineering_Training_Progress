ae82049bf48a44b1194f8d86be63af147ac9ec5f
KAKUNURI SRILAKSHMI <139469183+Kakunuri-Srilakshmi@users.noreply.github.com>
2025-09-19 17:25:26 +0530

    Delete Sep - 19  / E-Commerce_Order_Management_System/json_handler.py

--- Sep - 19  / E-Commerce_Order_Management_System/json_handler.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_orders(filename):
-    with open(filename, "r") as file:
-        return json.load(file)
-
-def save_orders(filename, orders):
-    with open(filename, "w") as file:
-        json.dump(orders, file, indent=2)
